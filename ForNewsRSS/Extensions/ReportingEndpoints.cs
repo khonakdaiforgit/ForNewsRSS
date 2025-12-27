c49f60d2ed8cf4b54dcd96b9cadd2a3bded8e76f
Khonakdai <khonakdarigithub@gmail.com>
2025-12-28 00:35:40 +0330

    fix and Clean 10 rss

--- ForNewsRSS/Extensions/ReportingEndpoints.cs
+++ ForNewsRSS/Extensions/ReportingEndpoints.cs
@@ -21,7 +21,7 @@ namespace ForNewsRSS.Extensions
                     .ToListAsync();
 
                 var sb = new StringBuilder();
-                sb.AppendLine("Server Running V:1.103");
+                sb.AppendLine("Server Running V:1.104");
                 sb.AppendLine();
 
                 // === جدول آمار کلی منابع ===
