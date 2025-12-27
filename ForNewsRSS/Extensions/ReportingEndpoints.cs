c9a2571bb9a7a67c6e2c429de6478922e4b75ffd
Khonakdai <khonakdarigithub@gmail.com>
2025-12-27 19:24:03 +0000

    new fo NBC

--- ForNewsRSS/Extensions/ReportingEndpoints.cs
+++ ForNewsRSS/Extensions/ReportingEndpoints.cs
@@ -21,7 +21,7 @@ namespace ForNewsRSS.Extensions
                     .ToListAsync();
 
                 var sb = new StringBuilder();
-                sb.AppendLine("Server Running V:1.101");
+                sb.AppendLine("Server Running V:1.102");
                 sb.AppendLine();
 
                 // === جدول آمار کلی منابع ===
